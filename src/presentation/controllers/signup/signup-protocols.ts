ef2c700a8dd5562757494d5c4742d37c6dfb6b3b
DanielMedeiros <kabal79@gmail.com>
2021-06-04 09:56:37 -0300

    refactor: change mocking strategy

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols'
+export * from '../../protocols/email-validator'
+export * from '../../../domain/usecasos/add-accountt'
+export * from '../../../domain/model/account'
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,6 @@
-import { HttpRequest, HttpResponse, EmailValidator, Controller } from '../protocols'
-import { MissingParamError, InvalidParamError } from '../errors'
-import { badRequest, serveError } from '../helpers/http-helper'
-import { AddAccount } from '../../domain/usecasos/add-accountt'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, serveError } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
